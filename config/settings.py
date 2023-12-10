87382edf6f53156ef107d33e4a973169087f19d0
IslombekOrifov <islombekorifov0199@gmail.com>
2023-12-10 18:59:54 +0500

    added media root

--- config/settings.py
+++ config/settings.py
@@ -151,6 +151,7 @@ MODELTRANSLATION_LANGUAGES = ('uz', 'ru', 'en')
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'media'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
